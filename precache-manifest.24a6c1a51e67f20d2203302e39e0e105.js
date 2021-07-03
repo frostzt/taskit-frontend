0d7b5c169b6c65cf3a92f050eb2ba573793d35f2
frostzt <frostzt@users.noreply.github.com>
2021-07-03 05:46:40 +0000

    deploy: 638fa0062f4395cf11591f26db2c32e5e4fd3a58

---
+++ precache-manifest.24a6c1a51e67f20d2203302e39e0e105.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eca1c4ed114b5a577d68844c926eafd",
+    "url": "https://frostzt.github.io/taskit-frontend/index.html"
+  },
+  {
+    "revision": "178416ec41453c38263a",
+    "url": "https://frostzt.github.io/taskit-frontend/static/css/main.7f4a89a4.chunk.css"
+  },
+  {
+    "revision": "350359c3997177bed967",
+    "url": "https://frostzt.github.io/taskit-frontend/static/js/2.7a1e05c0.chunk.js"
+  },
+  {
+    "revision": "178416ec41453c38263a",
+    "url": "https://frostzt.github.io/taskit-frontend/static/js/main.13a4c0bf.chunk.js"
+  },
+  {
+    "revision": "519a06fbb66d3e925231",
+    "url": "https://frostzt.github.io/taskit-frontend/static/js/runtime~main.dc2d71f8.js"
+  }
+]);
